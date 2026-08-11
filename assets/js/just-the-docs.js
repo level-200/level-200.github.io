89de06d2b392091eeac237524870a1bf8ce3186c
level-200 <level200questions@gmail.com>
2026-08-11 17:46:21 -0500

    Attempt to patch search result display issue

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -421,11 +421,17 @@ function searchLoaded(index, docs) {
   const updateSearchFocus = function(evt) {
     const nextFocusedElement = evt.relatedTarget;
 
+    // If there's no related target (some browsers/platforms), hide search
+    if (!nextFocusedElement) {
+      hideSearch();
+      return;
+    }
+
     // Re-focusing on search bar - "keep focus"
     if (nextFocusedElement.id === 'search-input') return;
 
     // Re-focusing on the next search result element - "keep focus"
-    if (nextFocusedElement.classList.contains('search-result')) return;
+    if (nextFocusedElement.classList && nextFocusedElement.classList.contains('search-result')) return;
 
     // Otherwise, we're not focused on the search bar anymore. Hide!
     hideSearch();
